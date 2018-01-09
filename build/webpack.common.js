7dc813ac2998ef2b1d27c404eb0588d6996e1fbb
huanhulan <531971909@qq.com>
2018-01-10 02:07:12 +0800

    build(webpack): add postcss

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -2,61 +2,61 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = function (cssLoaderOptions, sassLoaderOptions) {
-  return {
-    entry: "./app/index.tsx",
-  
-    output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist",
-      publicPath: "/dist/"
-    },
+    return {
+        entry: "./app/index.tsx",
 
-    resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+        output: {
+            filename: "bundle.js",
+            path: __dirname + "/dist",
+            publicPath: "/dist/"
+        },
 
-    module: {
-      rules: [{
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
-      }, {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [{
-            loader: "typings-for-css-modules-loader",
-            options: cssLoaderOptions
-          },{
-            loader: "postcss-loader", options: {
-                sourceMap: true
-            }
-        }, {
-            loader: "sass-loader",
-            options: sassLoaderOptions
-          }]
-        })
-      }, {
-        test: /\.(png)$/i,
-        use: [{
-          loader: "file-loader",
-          options: {
-            name: "[hash].[ext]",
-            outputPath: "images/",
-            publicPath: "../"
-          }
-        }, {
-          loader: "image-webpack-loader"
-        }]
-      }, {
-        test: /\.modernizrrc\.json$/,
-        use: [{
-          loader: "modernizr-loader"
-        }, {
-          loader: "json-loader"
-        }]
-      }]
-    },
-     plugins: [
-      new ExtractTextPlugin("stylesheets/main.css")
-    ]
-  }
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".json"]
+        },
+
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            }, {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "typings-for-css-modules-loader",
+                        options: cssLoaderOptions
+                    }, {
+                        loader: "postcss-loader", options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: "sass-loader",
+                        options: sassLoaderOptions
+                    }]
+                })
+            }, {
+                test: /\.(png)$/i,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[hash].[ext]",
+                        outputPath: "images/",
+                        publicPath: "../"
+                    }
+                }, {
+                    loader: "image-webpack-loader"
+                }]
+            }, {
+                test: /\.modernizrrc\.json$/,
+                use: [{
+                    loader: "modernizr-loader"
+                }, {
+                    loader: "json-loader"
+                }]
+            }]
+        },
+        plugins: [
+            new ExtractTextPlugin("stylesheets/main.css")
+        ]
+    }
 };
