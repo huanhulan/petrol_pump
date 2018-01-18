09837683b58d92629572f609d97a17d9d5caf93e
huanhulan <531971909@qq.com>
2018-01-19 01:37:32 +0800

    fix(keypad.ts): fix wrong type assertation for `sBeep`

--- app/frp/keypad.ts
+++ app/frp/keypad.ts
@@ -28,7 +28,7 @@ function wireKeypad(sKeypad: StreamSink<Keys>|Stream<Keys>, sClear: Stream<Unit>
             }
         }).filterNotNull() as Stream<number>;
         cValue.loop(sKeyUpdate.orElse(sClear.map(u => 0)).hold(0));
-        const sBeep = sKeyUpdate.map(() => Unit.UNIT) as Stream<UNIT>;
+        const sBeep = sKeyUpdate.map(() => Unit.UNIT);
 
         return {sBeep, cValue}
     });
