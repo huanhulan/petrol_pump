57b00eca2f0fe4140f6104d9611d0ea4deb3d8eb
huanhulan <531971909@qq.com>
2018-01-21 21:27:06 +0800

    refactor(nozzle): fix tslint issues

--- app/components/nozzle_panel/index.tsx
+++ app/components/nozzle_panel/index.tsx
@@ -1,29 +1,29 @@
 import * as React from "react";
-import Nozzle from "./nozzle";
+import {nozzlePanelProps, nozzleProps} from "../../types";
 import * as nozzle1PNG from "./../../assets/images/nozzle1.png";
 import * as nozzle2PNG from "./../../assets/images/nozzle2.png";
 import * as nozzle3PNG from "./../../assets/images/nozzle3.png";
-import * as style from './style.scss';
-import {nozzlePanelProps, nozzleProps} from "../../types";
+import Nozzle from "./nozzle";
+import * as style from "./style.scss";
 
-class NozzlePanel extends React.Component<nozzlePanelProps,{}> {
+class NozzlePanel extends React.Component<nozzlePanelProps, {}> {
     private nozzles: nozzleProps[];
 
     constructor(props) {
         super(props);
         const srcs = [nozzle1PNG, nozzle2PNG, nozzle3PNG];
-        this.nozzles = ['FUEL1', 'FUEL2', 'FUEL3'].map((name, index) => {
+        this.nozzles = ["FUEL1", "FUEL2", "FUEL3"].map((name, index) => {
             return {
                 name,
                 src: srcs[index].toString(),
                 cNozzle: this.props.cNozzles[index],
                 sClick: this.props.sClicks[index],
-                cPriceLCD: this.props.cPriceLCDs[index]
-            }
+                cPriceLCD: this.props.cPriceLCDs[index],
+            };
         });
     }
 
-    render() {
+    public render() {
         return (
             <div className={style.nozzlePanel}>
                 {
--- app/components/nozzle_panel/nozzle/index.tsx
+++ app/components/nozzle_panel/nozzle/index.tsx
@@ -1,40 +1,41 @@
 import * as React from "react";
-import {UpDown, nozzleProps, nozzleState} from '../../../types';
-import LCD from './../../LCD';
-import * as style from './style.scss';
+import {nozzleProps, nozzleState, UpDown} from "../../../types";
+import LCD from "./../../LCD";
+import * as style from "./style.scss";
 
-class Nozzle extends React.Component<nozzleProps,nozzleState> {
+class Nozzle extends React.Component<nozzleProps, nozzleState> {
     constructor(props: nozzleProps) {
         super(props);
         this.state = {
-            direction: this.props.cNozzle.sample()
+            direction: this.props.cNozzle.sample(),
         };
     }
 
-    componentDidMount() {
-        this.props.cNozzle.listen((direction) => {
+    public componentDidMount() {
+        this.props.cNozzle.listen(direction => {
             this.setState({direction});
         });
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
+    public shouldComponentUpdate(nextProps, nextState) {
         return nextState.direction !== this.state.direction;
     }
 
-    onclick() {
-        this.props.sClick.send(this.state.direction === UpDown.UP ? UpDown.DOWN : UpDown.UP);
-    };
-
-    render() {
+    public render() {
         return (
             <div className={style.nozzle}>
                 <LCD name={this.props.name} cPresetLCD={this.props.cPriceLCD}/>
-                <button className={[style.nozzleButton,this.state.direction===UpDown.UP?style.lifted:''].join(' ')}
+                <button className={[style.nozzleButton, this.state.direction === UpDown.UP ? style.lifted : ""]
+                .join(" ")}
                         onClick={this.onclick.bind(this)}>
                     <img src={this.props.src} alt={this.props.name} className={style.nozzleImg}/>
                 </button>
             </div>
-        )
+        );
+    }
+
+    private onclick() {
+        this.props.sClick.send(this.state.direction === UpDown.UP ? UpDown.DOWN : UpDown.UP);
     }
 }
 
