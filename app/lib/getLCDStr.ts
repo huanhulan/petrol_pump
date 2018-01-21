dbbfaac1cadf6b39e8b866fde6670f0be378fd36
huanhulan <531971909@qq.com>
2018-01-21 22:31:44 +0800

    refactor(getLCDStr.ts): fix tslint issues

--- app/lib/getLCDStr.ts
+++ app/lib/getLCDStr.ts
@@ -1,7 +1,12 @@
 function getLCDStr(v: number|null, toFixed = 3) {
-    if (v === null) return '';
+    if (v === null) {
+        return "";
+    }
     const str = v.toFixed(toFixed);
-    if (str.length >= 3) return str;
-    return (new Array(4 - str.length)).join(' ') + str;
+    if (str.length >= 3) {
+        return str;
+    }
+    return (new Array(4 - str.length)).join(" ") + str;
 }
+
 export default getLCDStr;
