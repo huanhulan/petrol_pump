0bd5225afe76b89d3c218b9274705ee5b3cb3431
huanhulan <531971909@qq.com>
2018-01-21 18:24:19 +0800

    refactor(input_panel): remove unsed codes

--- app/components/input_panel/index.tsx
+++ app/components/input_panel/index.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
-import {Stream, CellLoop, Transaction, StreamSink} from  'sodiumjs'
-import {inputPanelInterface, Keys} from '../../types';
+import {inputPanelInterface} from '../../types';
 import LCD from '../LCD';
 import Keypad from './keypad';
 import * as style from './style.scss';
 
 class InputPanel extends React.Component<inputPanelInterface,{}> {
-    private getLCDStr(v: number|null) {
-        if (v === null) return '';
-        const str = v.toString();
-        if (str.length >= 3) return str + '.00';
-        return (new Array(4 - str.length)).join(' ') + str + '.00';
-    }
-
     constructor(props: inputPanelInterface) {
         super(props);
     }
