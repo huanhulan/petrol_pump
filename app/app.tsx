a9443c231046e85c5e5dbfae5966a347d65dac07
huanhulan <531971909@qq.com>
2018-01-21 18:56:34 +0800

    refactor(app.tsx): correct tslint errors

--- app/app.tsx
+++ app/app.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
-import * as ReactModal from 'react-modal';
-import {StreamSink, Unit} from 'sodiumjs';
-import PricePanel from './components/price_panel';
-import NozzlePanel from './components/nozzle_panel';
-import Audio from './components/audio';
-import LCD from './components/LCD';
-import InputPanel from './components/input_panel';
-import ModalContent from './components/modal_content';
-import {appPropsInterface, appStateInterface, saleInterface, Fuel} from './types';
+import * as ReactModal from "react-modal";
+import {StreamSink, Unit} from "sodiumjs";
+import Audio from "./components/audio";
+import InputPanel from "./components/input_panel";
+import LCD from "./components/LCD";
+import ModalContent from "./components/modal_content";
+import NozzlePanel from "./components/nozzle_panel";
+import PricePanel from "./components/price_panel";
+import {Fuel, appPropsInterface, appStateInterface, saleInterface} from "./types";
 import * as style from "./style/index.scss";
 
-class App extends React.Component<appPropsInterface,appStateInterface> {
-    sCloseModal: StreamSink<Unit>
+class App extends React.Component<appPropsInterface, appStateInterface> {
+    private sCloseModal: StreamSink<Unit>;
 
     constructor(props) {
         super(props);
@@ -19,28 +19,28 @@ class App extends React.Component<appPropsInterface,appStateInterface> {
         this.props.sSaleComplete.listen((sale: saleInterface) => {
             this.setState({
                 isModalOpen: true,
-                sale
+                sale,
             });
         });
         this.sCloseModal = this.props.csClearSale.sample() as StreamSink<Unit>;
     }
 
-    closeModal() {
+    private closeModal() {
         this.sCloseModal.send(Unit.UNIT);
         this.props.csClearSale.send(this.sCloseModal);
         this.setState({
             isModalOpen: false,
-            sale: null
+            sale: null,
         });
     }
 
-    render() {
+    private render() {
         return (
             <div className={style.app}>
                 <ReactModal
                     isOpen={this.state.isModalOpen}
-                    portalClassName={`${style.modalPortal} ${this.state.isModalOpen?style.active:''}`}
-                    overlayClassName={`${style.modalOverlay} ${this.state.isModalOpen?style.active:''}`}
+                    portalClassName={`${style.modalPortal} ${this.state.isModalOpen ? style.active : ""}`}
+                    overlayClassName={`${style.modalOverlay} ${this.state.isModalOpen ? style.active : ""}`}
                     className={`${style.modal} ${style.active}`}
                     shouldFocusAfterRender={true}
                     shouldCloseOnOverlayClick={true}
@@ -50,7 +50,16 @@ class App extends React.Component<appPropsInterface,appStateInterface> {
                     <a className={style.closeModal} onClick={this.closeModal.bind(this)}>
                         <svg viewBox="0 0 20 20" className={style.closeModalSvg}>
                             <path fill="#000000"
-                                  d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"/>
+                                  d="M15.898,\
+                                  4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,\
+                                  4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,\
+                                  0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,\
+                                  0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,\
+                                  0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,\
+                                  4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,\
+                                  0.492-0.203c0.273-0.273,0.273-0.715,\
+                                  0-0.986l-4.711-4.711l4.711-4.711C16.172,\
+                                  4.759,16.172,4.317,15.898,4.045z"/>
                         </svg>
                     </a>
                     {this.state.sale !== null
@@ -88,8 +97,6 @@ class App extends React.Component<appPropsInterface,appStateInterface> {
             </div>
         );
     }
-
-
 }
 
 export default App;
