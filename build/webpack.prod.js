63abe6b8cd8fc17e0d72809d991075f9d922e3d4
huanhulan <531971909@qq.com>
2018-11-03 00:48:39 +0800

    build(build/): refine prod config and dev development experience

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,28 +1,30 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const common = require("./webpack.common.js");
 const merge = require("webpack-merge");
 const webpack = require('webpack');
+const WebpackDeepScopeAnalysisPlugin = require('webpack-deep-scope-plugin').default;
+const common = require("./webpack.common.js");
 
 const cssLoaderOptions = {
     camelCase: true,
     modules: true,
     minimize: true,
     namedExport: true,
-    scss: true
+    scss: true,
+    localIdentName: "[hash:base64:5]",
 };
 
 const sassLoaderOptions = {};
 
 module.exports = merge(common(cssLoaderOptions, sassLoaderOptions), {
     plugins: [
+        new WebpackDeepScopeAnalysisPlugin(),
         new CleanWebpackPlugin(['dist'], {
             root: __dirname
         }),
-        new webpack.optimize.UglifyJsPlugin(),
         new webpack.DefinePlugin({
             "process.env": {
                 "NODE_ENV": JSON.stringify("production")
             }
-        })
+        }),
     ]
 });
